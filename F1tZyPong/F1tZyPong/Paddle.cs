8750af7312d223d262047ef29ec5f23b6542cb5b
F1tZ81 <f1tz81@gmail.com>
2013-09-02 15:52:25 -0400

    Stated adding user control to the Paddle

--- F1tZyPong/F1tZyPong/Paddle.cs
+++ F1tZyPong/F1tZyPong/Paddle.cs
@@ -2,11 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace F1tZyPong
 {
     public class Paddle : Entity
     {
+        KeyboardState KeyState;
         public Paddle() : base() { }
+
+        /// <summary>
+        /// Load the image for the paddle
+        /// </summary>
+        /// <param name="path">the path of the paddle to load</param>
+        public override void LoadContent(Texture2D ContentToLoad)
+        {
+            this.Image = ContentToLoad;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            KeyState = Keyboard.GetState();
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                GameState.CurrentState = States.Exit;
+            if (KeyState.IsKeyDown(Keys.Down)) { this.Volcity.Y = 1; }
+        }
+
+
     }
 }
