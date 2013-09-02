6d84b2e0fa46f9f5e28aae774f15debbfdf62112
F1tZ81 <f1tz81@gmail.com>
2013-09-02 15:14:32 -0400

    Added Pong Sln
    the first in many commits for Pong Clone

---
+++ F1tZyPong/F1tZyPong/Entity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace F1tZyPong
+{
+    public class Entity
+    {
+        Texture2D Image = null;
+        Vector2 Posistion = Vector2.Zero;
+        Vector2 Volcity = Vector2.Zero;
+        Rectangle BoundingBox = new Rectangle();
+
+        public Entity() { }
+
+        public Entity(Texture2D imageToSet, Vector2 position)
+        {
+            Image = imageToSet;
+            Posistion = position;
+        }
+
+        public abstract void LoadContent(string path) { }
+
+        public abstract void UnloadContent() { }
+
+        public abstract void Update() { }
+
+        public abstract void Draw() { }
+    }
+}
---
+++ F1tZyPong/F1tZyPong/Paddle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace F1tZyPong
+{
+    public class Paddle : Entity
+    {
+        public Paddle() : base() { }
+    }
+}
